75f7b743096730b5ee4d8613ccd1a52108e149ed
Abed Nganga <ngangaabed@gmail.com>
2023-08-19 08:22:23 +0300

    Completed Assignmemt

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [x for x in num_list if x%2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations =[sentence + '!' for sentence in sentence_list]
+    return exclamations
+
+
+
+
+
+input_sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = return_evens(input_sequence)
+print(result)
+
+input_sentences = ["Hello", "How are you", "Python is fun"]
+result4 = make_exclamation(input_sentences)
+print(result4)
